97866fef2e9135d2a3439b7b3d2ad99818432d67
leonel0927 <leonelosorio223@gmail.com>
2026-02-22 21:38:26 -0600

    PRACTICA SSH

--- windows/vim.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item -Force Z:\windows\vim.exe
-# Obtener el PATH actual
-$oldPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
-
-# Filtrar y quitar las rutas que contengan 'Vim' o 'Z:\windows' o 'C:\editor'
-$newPath = ($oldPath -split ';' | Where-Object { $_ -notmatch "Vim" -and $_ -notmatch "Z:\\windows" -and $_ -notmatch "C:\\editor" }) -join ';'
-
-# Guardar el PATH limpio
-[Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
